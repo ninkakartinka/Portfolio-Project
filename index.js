98122088e42c895eb934f7683ce00db786f096bc
ninkakartinka <ninkakartinka98@gmail.com>
2017-05-19 12:50:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+$(function(){
+  $.scrollIt();
+});
+
+$(function() {
+$('#container').twentytwenty();
+});
+
+$(function() {
+$('#container1').twentytwenty();
+});
+
+$(function() {
+$('#container2').twentytwenty();
+});
+
+$(function() {
+$('#container3').twentytwenty();
+});
+
+$(function() {
+$('#container4').twentytwenty();
+});
+
+$(function() {
+$('#container5').twentytwenty();
+});
+
+$(function() {
+$('#container6').twentytwenty();
+});
